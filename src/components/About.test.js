c7e53358fb34890f0a922b7549b5c1108f15d9a2
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-03-13 16:29:24 -0500

    get jest working on test

---
+++ src/components/About.test.js
@@ -0,0 +1,18 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import renderer from 'react-test-renderer';
+import { MemoryRouter } from 'react-router-dom';
+import { About } from '.';
+
+it('changes the class when hovered', () => {
+  const about = renderer.create(
+    <MemoryRouter>
+      <About />
+    </MemoryRouter>
+  );
+
+  let tree = about.toJSON();
+  expect(tree).toMatchSnapshot();
+});
