c16ed529394226cf79fe74755a06650f60de7506
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-03-19 13:29:08 -0500

    cleaning up routes and adding

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Home, About, Portfolio, Snake, NotFound, JuiceboxDoc } from '.';
 import { JuiceboxMain } from '../fullReactProjects/juicebox/components';
+import { StrangerMain } from '../fullReactProjects/stranger/src/components';
 
 function Main() {
   return (
@@ -11,6 +12,7 @@ function Main() {
         <Route path="/about" element={<About />} />
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/snake" element={<Snake />} />
+        <Route path="/stranger/*" element={<StrangerMain />} />
         <Route path="/juicebox/*" element={<JuiceboxMain />} />
         <Route path="/api/juicebox" element={<JuiceboxDoc />} />
         <Route path="*" element={<NotFound />} />
