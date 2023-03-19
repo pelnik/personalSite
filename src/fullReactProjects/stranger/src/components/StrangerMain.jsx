77f391441c5fbacb8a5079383d0b15d8b385e24c
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-03-19 13:22:59 -0500

    still cleaning up files, css

--- src/fullReactProjects/stranger/src/components/StrangerMain.jsx
+++ src/fullReactProjects/stranger/src/components/StrangerMain.jsx
@@ -1,21 +1,20 @@
-import React, { useEffect, useState } from "react";
-import { Routes, Route } from "react-router-dom";
-import { Navbar, Posts } from "./";
-import { checkLocalStorageToken } from "../utils";
+import React, { useEffect, useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Navbar, Posts } from '.';
+import { checkLocalStorageToken } from '../utils';
 
 function Main() {
   const [userToken, setUserToken] = useState(null);
-  const [postFilter, setPostFilter] = useState("");
+  const [postFilter, setPostFilter] = useState('');
 
   useEffect(() => {
     setUserToken(checkLocalStorageToken());
   }, []);
 
-  useEffect(() => {
-  }, [userToken]);
+  useEffect(() => {}, [userToken]);
 
   return (
-    <div id="main">
+    <div id="strangerMain">
       <Navbar
         userToken={userToken}
         setUserToken={setUserToken}
