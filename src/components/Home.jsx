a0109ebba66bc1ad45fd5a86766b6f3afa20cfe0
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-03-23 17:11:42 -0500

    API documentation and typo fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,15 +19,15 @@ function Home() {
               <p>
                 👉 My name is Matt Pelnik and welcome to my personal site.
                 I&apos;m a web software engineer based out of Tulsa, OK. I have
-                an engineering background with and have worked in tech,
-                healthcare, and energy. Please have a look at my portfolio and
-                about section to learn more about me.
+                an engineering background and have worked in tech, healthcare,
+                and energy. Please have a look at my portfolio and about section
+                to learn more about me.
               </p>
               <p>
-                👉 I&apos;ve worked previously with Oracle in their healthcare
-                group as a business analyst building low code automation
-                systems. I wanted to move my interest to a career and code
-                fulltime, and I&apos;d love to talk.
+                👉 I have worked previously with Oracle in their healthcare
+                group building low code automation systems. I wanted to turn my
+                interest into a career and code full time, and I would love to
+                talk.
               </p>
             </div>
           </div>
--- src/components/apiDocs/JuiceboxDoc.jsx
+++ src/components/apiDocs/JuiceboxDoc.jsx
@@ -18,9 +18,9 @@ function JuiceboxDoc() {
               <h1>Juicebox API Documentation</h1>
               <p className="jbText">
                 Thanks for looking into my API documentation! The following will
-                showcase the various paths that can be accessed with my API.
-                Throughout, I will give example you can copy to your Terminal in
-                cURL, so that you can test my API yourself.
+                summarize <u>a few</u> of the various paths that can be accessed
+                with my API. Throughout, I will give example you can copy to
+                your Terminal in cURL, so that you can test my API yourself.
               </p>
               <p className="jbText">
                 First, the visual style of the documentation will represent
@@ -59,7 +59,61 @@ function JuiceboxDoc() {
                 &quot;name&quot;: &quot;&#91;Billy&#93;&quot;,
                 &quot;location&quot;: &quot;&#91;Kansas&#93;&quot; &#125;&apos;
               </p>
-              <p className="jbText">More to come!</p>
+              <hr className="api-rule" />
+              <h2>Login</h2>
+              <p className="jbCode">pelnik.dev/api/juicebox/login</p>
+              <p className="jbText">
+                You can get posts without a user token, but to do most things,
+                first you will need a user token. To login a user, you will need
+                to send a username and password.
+              </p>
+              <p className="jbCurl">
+                curl https://pelnik.dev/api/juicebox/users/login -X POST -H
+                &quot;Content-Type: application/json&quot; -d &apos;&#123;
+                &quot;username&quot;: &quot;&#91;FunGuy23&#93;&quot;,
+                &quot;password&quot;: &quot;&#91;verysecure&#93;&quot;
+                &#125;&apos;
+              </p>
+              <hr className="api-rule" />
+              <h2>Get Posts</h2>
+              <p className="jbCode">pelnik.dev/api/juicebox/posts</p>
+              <p className="jbText">
+                You can get posts without a user token, but if you include a
+                token the API will send you a flag with isAuthor to help the
+                front end identify the logged in user's posts. Get posts doesn't
+                require a body.
+              </p>
+              <p className="jbCurl">
+                curl https://pelnik.dev/api/juicebox/posts -H
+                &quot;Content-Type: application/json&quot; &#91;-H
+                &quot;Authorization: Bearer &#91;token&#93;&quot;&#93;
+              </p>
+              <hr className="api-rule" />
+              <h2>And Many More!</h2>
+              <a
+                href="https://github.com/pelnik/JuiceboxFrontEnd"
+                target="_blank"
+                className="jbLink"
+              >
+                Click here to take look around at the front end to see all API's
+                employed.
+              </a>
+              <a
+                href="https://github.com/pelnik/juicebox"
+                target="_blank"
+                className="jbLink"
+              >
+                Or if you really want to dig into it, look at the back end repo
+                I created directly!
+              </a>
+              <a
+                href="https://github.com/pelnik/personalSite"
+                className="jbLink"
+                target="_blank"
+              >
+                All of these individual repos were modified and added to my
+                personal site API.
+              </a>
             </div>
           </div>
         </div>
