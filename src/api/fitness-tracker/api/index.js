5e4becd9a8e02e59230f5fd826c987dad3092772
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-03-24 10:19:32 -0500

    correct APi navigation bug

--- src/api/fitness-tracker/api/index.js
+++ src/api/fitness-tracker/api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fitnessRouter = express.Router();
 const jwt = require('jsonwebtoken');
+const path = require('path');
 const { getUserById } = require('../db');
 const { JWT_SECRET } = process.env;
 
