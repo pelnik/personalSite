d5e4295fbfab0654abb90e146df2769a5a0bfb22
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-04-14 09:17:55 -0500

    need to switch branches, no idea whast I did last

--- src/components/Utilities/ButtonSpecial.jsx
+++ src/components/Utilities/ButtonSpecial.jsx
@@ -1,12 +1,74 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 function ButtonSpecial() {
+  const parentRef = useRef({});
+  const buttonRef = useRef({});
 
-  function
+  const parentOriginalPadding = useRef(0);
+  const buttonOriginalPadding = useRef(0);
+
+  const [parentPadding, setParentPadding] = useState(null);
+  const [buttonPadding, setButtonPadding] = useState(null);
+
+  function buttonSpecialClick(evt) {}
+
+  function updateEachPadding(objectToUpdate, paddingObject) {
+    const tempObject = {};
+    tempObject.paddingTop = paddingObject.paddingTop;
+    tempObject.paddingRight = paddingObject.paddingRight;
+    tempObject.paddingBottom = paddingObject.paddingBottom;
+    tempObject.paddingLeft = paddingObject.paddingLeft;
+
+    console.log('objectToUpdate', objectToUpdate);
+    objectToUpdate.current = tempObject;
+    console.log('objectToUpdate', objectToUpdate);
+  }
+
+  function addStyle(padding) {
+    const styleObject = {};
+    styleObject.backgroundColor = 'yellow';
+
+    if (padding) {
+      styleObject.paddingTop = padding.paddingTop;
+      styleObject.paddingRight = padding.paddingRight;
+      styleObject.paddingBottom = padding.paddingBottom;
+      styleObject.paddingLeft = padding.paddingLeft;
+    }
+
+    return styleObject;
+  }
+
+  function handleMouseEnter() {
+    setParentPadding({});
+  }
+
+  useEffect(() => {
+    updateEachPadding(
+      parentOriginalPadding,
+      window.getComputedStyle(parentRef.current)
+    );
+
+    updateEachPadding(
+      buttonOriginalPadding,
+      window.getComputedStyle(buttonRef.current)
+    );
+  }, []);
 
   return (
-    <div onClick={buttonSpecialClick} className="button-special">
-      <button className="navbar-button">About</button>
+    <div
+      onClick={buttonSpecialClick}
+      ref={parentRef}
+      className="button-special"
+      style={addStyle(parentPadding)}
+      onMouseEnter={handleMouseEnter}
+    >
+      <button
+        ref={buttonRef}
+        className="navbar-button"
+        style={addStyle(buttonPadding)}
+      >
+        About
+      </button>
     </div>
   );
 }
