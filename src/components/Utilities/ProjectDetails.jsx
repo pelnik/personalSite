dff5ee93d8f31c0e47ba9ef28d9d46eae3e9d036
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-04-24 11:15:28 -0500

    CSS'ing image

--- src/components/Utilities/ProjectDetails.jsx
+++ src/components/Utilities/ProjectDetails.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 function ProjectDetails({ project }) {
   return (
     <div className="project-content">
-      <img src={project.img} alt={project.img_alt} />
+      <div className="img-container">
+        <img src={project.img} alt={project.img_alt} />
+      </div>
       <div className="project-description">
         {project.description.map((section, idx) => {
           return <p key={idx}>{section}</p>;
