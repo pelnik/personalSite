786b899667e067ea20c4657cf2bfc0e2463c7600
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-04-29 14:13:20 -0500

    lifted ref logic to parent for mouse enter

--- src/components/Utilities/ProjectImages.jsx
+++ src/components/Utilities/ProjectImages.jsx
@@ -1,75 +1,10 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 
-function ProjectImages({ project, projectWidth }) {
-  const images = project.img;
-
-  const activeImages = useRef(
-    images.map((image, idx) => {
-      return idx === 0;
-    })
-  );
-  const imageRef = useRef(new Map());
-  const scrollingInterval = useRef([]);
-  console.log('each render active image', activeImages);
-
-  function scrollToImageIdx(idx) {
-    const map = imageRef.current;
-    const node = map.get(idx);
-    node.scrollIntoView({
-      behavior: 'smooth',
-      block: 'nearest',
-      inline: 'center',
-    });
-
-    console.log(
-      'new array to set ',
-      activeImages.current.map((image, newIdx) => {
-        return idx === newIdx;
-      })
-    );
-
-    activeImages.current = [...activeImages.current].map((image, newIdx) => {
-      return idx === newIdx;
-    });
-  }
-
-  function getCurrentImage() {
-    console.log('activeImages inside of getCurrent', activeImages.current);
-    console.log(
-      'get current image',
-      activeImages.current.findIndex((imageActive) => imageActive)
-    );
-    return activeImages.current.findIndex((imageActive) => imageActive);
-  }
-
-  const onMouseEnter = (evt) => {
-    const intervalID = setInterval(() => {
-      console.log('hover scope active images', activeImages.current);
-      const currentIdx = getCurrentImage();
-      const nextIdx =
-        currentIdx + 1 > activeImages.current.length - 1 ? 0 : currentIdx + 1;
-
-      console.log('next idx, passed into funtion', nextIdx);
-
-      scrollToImageIdx(nextIdx);
-    }, 2000);
-    scrollingInterval.current.push(intervalID);
-  };
-
-  const onMouseLeave = (evt) => {
-    scrollingInterval.current.forEach((intervalID) => {
-      clearInterval(intervalID);
-    });
-
-    scrollingInterval.current = [];
-  };
+function ProjectImages({ project, projectWidth, imageRef, images }) {
+  console.log('projectImages', images);
 
   return (
-    <div
-      onMouseEnter={onMouseEnter}
-      onMouseLeave={onMouseLeave}
-      className="img-container"
-    >
+    <div className="img-container">
       <div className="img-scrolling">
         {images.map((image, idx) => {
           return (
--- src/components/Utilities/ProjectWrapper.jsx
+++ src/components/Utilities/ProjectWrapper.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { ProjectDescription, ProjectImages } from '..';
 
 import { Link } from 'react-router-dom';
@@ -13,8 +13,75 @@ function ProjectWrapper({
   projectWidth,
   setDescriptionTracker,
 }) {
+  const images = project.img;
+
+  const imageRef = useRef(new Map());
+
+  const activeImages = useRef(
+    images.map((image, idx) => {
+      return idx === 0;
+    })
+  );
+  const scrollingInterval = useRef([]);
+
+  function scrollToImageIdx(idx) {
+    const map = imageRef.current;
+    const node = map.get(idx);
+    node.scrollIntoView({
+      behavior: 'smooth',
+      block: 'nearest',
+      inline: 'center',
+    });
+
+    console.log(
+      'new array to set ',
+      activeImages.current.map((image, newIdx) => {
+        return idx === newIdx;
+      })
+    );
+
+    activeImages.current = [...activeImages.current].map((image, newIdx) => {
+      return idx === newIdx;
+    });
+  }
+
+  function getCurrentImage() {
+    console.log('activeImages inside of getCurrent', activeImages.current);
+    console.log(
+      'get current image',
+      activeImages.current.findIndex((imageActive) => imageActive)
+    );
+    return activeImages.current.findIndex((imageActive) => imageActive);
+  }
+
+  const onMouseEnter = (evt) => {
+    const intervalID = setInterval(() => {
+      console.log('hover scope active images', activeImages.current);
+      const currentIdx = getCurrentImage();
+      const nextIdx =
+        currentIdx + 1 > activeImages.current.length - 1 ? 0 : currentIdx + 1;
+
+      console.log('next idx, passed into funtion', nextIdx);
+
+      scrollToImageIdx(nextIdx);
+    }, 2000);
+    scrollingInterval.current.push(intervalID);
+  };
+
+  const onMouseLeave = (evt) => {
+    scrollingInterval.current.forEach((intervalID) => {
+      clearInterval(intervalID);
+    });
+
+    scrollingInterval.current = [];
+  };
+
   return project.external ? (
-    <div className="individual-project">
+    <div
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      className="individual-project"
+    >
       <a
         className="project-link-container"
         href={project.link}
@@ -22,7 +89,12 @@ function ProjectWrapper({
         rel="noreferrer"
         ref={firstProjectID === project.id ? projectElement : null}
       >
-        <ProjectImages project={project} projectWidth={projectWidth} />
+        <ProjectImages
+          project={project}
+          projectWidth={projectWidth}
+          imageRef={imageRef}
+          images={images}
+        />
         <ProjectDescription
           project={project}
           projectWidth={projectWidth}
@@ -40,13 +112,22 @@ function ProjectWrapper({
       </a>
     </div>
   ) : (
-    <div className="individual-project">
+    <div
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      className="individual-project"
+    >
       <Link
         className="project-link-container"
         ref={firstProjectID === project.id ? projectElement : null}
         to={project.link}
       >
-        <ProjectImages project={project} projectWidth={projectWidth} />
+        <ProjectImages
+          project={project}
+          projectWidth={projectWidth}
+          imageRef={imageRef}
+          images={images}
+        />
         <ProjectDescription
           project={project}
           projectWidth={projectWidth}
