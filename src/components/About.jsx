c1c7d3fbd458663133edcf28a555f93f6d751f99
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-07-21 17:12:54 -0500

    added pic and some other stuff

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,10 +3,14 @@ import React from 'react';
 function About() {
   return (
     <div id="about-parent">
-      <p>
-        Hi there, thank you for visiting my site! I'm an analyst based out of
-        Tulsa, Oklahoma. Take a look at my web development projects below 👇
-      </p>
+      <div id="about-pic-flex">
+        <img className="personal-pic" src="/Media/main/personal_pic.jpg" />
+        <p>
+          Hi there, thank you for visiting my site! I'm an analyst/developer
+          based out of Tulsa, Oklahoma. Take a look at my web development
+          projects below 👇
+        </p>
+      </div>
     </div>
   );
 }
