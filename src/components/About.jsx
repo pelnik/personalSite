43867053b3ae0232adfe0c8aa98ea1e935426595
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-07-23 16:05:34 -0500

    juicebox api and wincrease width of projects

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ function About() {
     <div id="about-parent">
       <div id="about-pic-flex">
         <img className="personal-pic" src="/Media/main/personal_pic.jpg" />
-        <p>
+        <p className="about-text">
           Hi there, thank you for visiting my site! I'm an analyst/developer
           based out of Tulsa, Oklahoma. Take a look at my web development
           projects below 👇
