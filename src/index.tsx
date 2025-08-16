39a02a8fd9614544b02cadcc533694a88c353e81
Matthew Pelnik <matthew.pelnik@icloud.com>
2025-08-16 17:15:33 -0500

    conversions to TS

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Main } from './components';
+
+const container = document.getElementById('app')!;
+const root = createRoot(container);
+root.render(
+  <StrictMode>
+    <Router>
+      <Main />
+    </Router>
+  </StrictMode>
+);
