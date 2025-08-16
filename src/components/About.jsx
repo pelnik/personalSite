d2659f371d3e6699fcf52b39b46b87c772a1de29
Matthew Pelnik <matthew.pelnik@icloud.com>
2025-08-16 16:30:55 -0500

    about page update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,21 +6,11 @@ function About() {
       <div id="about-pic-flex">
         <img className="personal-pic" src="/Media/main/personal_pic.jpg" />
         <p className="about-text">
-          👋 Hi there, thank you for visiting my site! I'm a data analyst and
-          web developer based out of Tulsa, Oklahoma. I specialize in data
-          analytics using SAS, SQL, and Power BI. I am currently working a
-          federal contract using statistical and medical analyses to identify
-          fraudulent providers billing Medicare.
-          <br />
-          <br />
-          🗃️{' '}
-          <a
-            target="_blank"
-            className="resume-link"
-            href="/Resume Matthew Pelnik.pdf"
-          >
-            Take a look at my resume!
-          </a>
+          👋 Hi there, thank you for visiting my site! I’m Matt, a Business
+          Intelligence Analyst in Institutional Research at Tulsa Community
+          College. I use Python and SQL to turn institutional data into reports
+          and analyses that support student and faculty success, operations, and
+          compliance.
           <br />
           <br />
           👇 This site was built by me from scratch with React and Express. View
