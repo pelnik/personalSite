49d9be7772ddd11b6ca642f00b8ffdcdae115208
Matthew Pelnik <matthew.pelnik@icloud.com>
2026-03-17 16:38:34 -0500

    chore: stop tracking build folder

--- build/portfolio/DeucesWild/game.js
+++
@@ -1,5 +0,0 @@
-// Import classes
-import DeucesWildGame from './modules/DeucesWildGame.js';
-
-// For now, we don't need to call the object so no assignment
-new DeucesWildGame(false); // eslint-disable-line
